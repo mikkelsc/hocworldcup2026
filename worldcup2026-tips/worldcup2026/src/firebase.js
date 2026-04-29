ce5f75b8e9c2a2a3c1b238c55650f43637e68add
mikkelsc <66221131+mikkelsc@users.noreply.github.com>
2026-04-29 16:22:49 +0200

    Update firebase.js

--- worldcup2026-tips/worldcup2026/src/firebase.js
+++ worldcup2026-tips/worldcup2026/src/firebase.js
@@ -14,12 +14,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBhpx8jJr2NmBhdFkgJPHxvHuxfIqdJ3yo",
+  authDomain: "worldcup2026-tips.firebaseapp.com",
+  projectId: "worldcup2026-tips",
+  storageBucket: "worldcup2026-tips.firebasestorage.app",
+  messagingSenderId: "561408695284",
+  appId: "1:561408695284:web:51eb05a0b3a7d91b96b7a1"
 };
 
 const app = initializeApp(firebaseConfig);
